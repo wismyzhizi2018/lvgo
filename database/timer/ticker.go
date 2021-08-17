c5638be9eed4ea7df23b24e6becbf47139527659
wismyzhizi <wismyzhizi@163.com>
2021-08-17 15:23:16 +0800

    timer和ticker学习

---
+++ database/timer/ticker.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gookit/color"
+	"time"
+)
+
+//类似js 中的setInterval函数
+
+func main() {
+	t := time.NewTicker(2 * time.Second)
+	//	t1 := time.NewTimer(2 * time.Second)
+	for {
+		time := <-t.C
+		//time2 := <-t1.C
+		color.Info.Println(time.Format("2006-01-01 15:04:05"))
+
+		//color.Danger.Println(time2.Format("2006-01-01 15:04:05"))
+	}
+}
---
+++ database/timer/timer.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gookit/color"
+	"time"
+)
+
+//类似js 中的setTimeout函数
+func main() {
+	//t := time.NewTicker(2 * time.Second)
+	t1 := time.NewTimer(2 * time.Second)
+
+	//time := <-t.C
+	time2 := <-t1.C
+	//color.Info.Println(time.Format("2006-01-01 15:04:05"))
+
+	color.Danger.Println(time2.Format("2006-01-01 15:04:05"))
+
+}

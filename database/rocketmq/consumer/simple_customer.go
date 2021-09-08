8fa200c80d625a7f282456f22614b38082031f0a
wismyzhizi <wismyzhizi@163.com>
2021-09-08 10:32:04 +0800

    rocketmq接入v2

---
+++ database/rocketmq/consumer/simple_customer.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/apache/rocketmq-client-go/v2"
+	"github.com/apache/rocketmq-client-go/v2/consumer"
+	"github.com/apache/rocketmq-client-go/v2/primitive"
+	"time"
+)
+
+func main() {
+	c, _ := rocketmq.NewPushConsumer(
+		consumer.WithNameServer([]string{"61.171.40.222:9876"}),
+		consumer.WithGroupName("mxshop"),
+	)
+	_ = c.Subscribe("hellomq", consumer.MessageSelector{}, callback)
+	_ = c.Start()
+	time.Sleep(time.Second * 3600)
+}
+
+func callback(cxt context.Context, messages ...*primitive.MessageExt) (consumer.ConsumeResult, error) {
+	for i, message := range messages {
+		fmt.Printf("Message=%d ===%s \r\n", i, message)
+	}
+	return consumer.ConsumeSuccess, nil
+}

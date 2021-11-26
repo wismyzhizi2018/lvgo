ce192f807d83ca8310bc486e6ed7170b79a06c64
wismyzhizi <wismyzhizi@163.com>
2021-11-26 18:02:16 +0800

    整理监控代码

--- database/monitoring/main.go
+++ database/monitoring/main.go
@@ -2,12 +2,6 @@ package main
 
 import (
 	"bytes"
-	"crypto/hmac"
-	"crypto/sha256"
-	_ "embed"
-	"encoding/base64"
-	"encoding/hex"
-	"errors"
 	"fmt"
 	"github.com/CodyGuo/dingtalk"
 	"github.com/golang/glog"
@@ -18,24 +12,11 @@ import (
 	"github.com/namsral/flag"
 	"github.com/robfig/cron"
 	"github.com/spf13/viper"
-	"io/ioutil"
-	"net/http"
 	"order/config"
 	"os"
 	"path"
-	"strings"
 	"time"
 )
-
-var httpClient http.Client
-var retry int
-
-func init() {
-	httpClient = http.Client{
-		Timeout: 10 * time.Second,
-	}
-	retry = 3
-}
 
 type YamlConfig struct {
 	Version int      `yaml:"version"`
@@ -53,15 +34,6 @@ type Config struct {
 	Desc     string `yaml:"desc"`
 	LastTime string `yaml:"last"`
 	NowTime  string `yaml:"now"`
-}
-
-type DingConfig struct {
-	Msgtype  string   `json:"msgtype"`
-	Markdown Markdown `json:"markdown"`
-}
-type Markdown struct {
-	Title string `json:"title"`
-	Text  string `json:"text"`
 }
 
 //go:generate goversioninfo -icon=resource/icon.ico -manifest=resource/goversioninfo.exe.manifest
@@ -105,7 +77,7 @@ func main() {
 				lastTime, _ := FileLastUpdateTime(item.Path)
 				diffTime := nowTime - lastTime
 				if diffTime > item.Time {
-					fmt.Printf("%s大于设置时间%v秒\n", item.Desc, item.Time)
+					fmt.Printf("%s大于设置时间%v秒\n", item.Path, item.Time)
 					//需要获取推送的订单信息
 					item.LastTime = timeToData(lastTime)
 					item.NowTime = timeToData(nowTime)
@@ -141,9 +113,7 @@ func main() {
 			if err := dt.RobotSendMarkdown(markdownTitle, markdownText); err != nil {
 				glog.Fatal(err)
 			}
-			fmt.Println(dt)
 		}
-		fmt.Println(content)
 	})
 	if err != nil {
 		fmt.Println(err)
@@ -156,23 +126,10 @@ func main() {
 	}
 
 }
-func EncodeSHA256(data string, secret string) string {
-	h := hmac.New(sha256.New, []byte(secret))
-	h.Write([]byte(data))
-	sum := h.Sum(nil)
-	message1 := base64.StdEncoding.EncodeToString(sum)
 
-	message2 := base64.URLEncoding.EncodeToString([]byte(message1))
-	//uv := url.Values{}
-	//uv.Add("0", message1)
-	//message2 := uv.Encode()[2:]
-	return message2
-}
-func hmacSha256(data string, secret string) string {
-	h := hmac.New(sha256.New, []byte(secret))
-	h.Write([]byte(data))
-	return hex.EncodeToString(h.Sum(nil))
-}
+//
+// timeToData  时间戳转换时间
+//  @param path 文件路径
 func timeToData(timestamp int64) string {
 	timeFormat := "2006-01-02 15:04:05"
 	// 时间戳转日期
@@ -180,6 +137,9 @@ func timeToData(timestamp int64) string {
 	return t3.Format(timeFormat)
 }
 
+//
+// FileLastUpdateTime  获取文件最后时间
+//  @param path 文件路径
 func FileLastUpdateTime(path string) (int64, error) {
 	f, err := os.Open(path)
 	defer f.Close()
@@ -192,6 +152,10 @@ func FileLastUpdateTime(path string) (int64, error) {
 	}
 	return fi.ModTime().Unix(), nil
 }
+
+//
+// Exists  判断文件是否存在
+//  @param path 文件路径
 func Exists(path string) (bool, error) {
 	_, err := os.Stat(path)
 	if err == nil {
@@ -203,6 +167,7 @@ func Exists(path string) (bool, error) {
 	return true, err
 }
 
+//
 // InitNACOS
 // @title  从配置中心读取配置文件
 // @description  从配置中心读取配置文件
@@ -283,74 +248,4 @@ func InitNACOS() {
 	if err := viper.Unmarshal(&c); err != nil {
 		panic(err)
 	}
-}
-
-//
-//  Curl 发送http请求
-//  @Description: 发送http请求
-//  @param method 请求方法
-//  @param url
-//  @param query 请求参数 支持（string,map[string]string）
-//  @param headers 请求头
-//  @return []byte
-//  @return error
-//
-func Curl(method, url string, query interface{}, headers map[string]string) ([]byte, error) {
-	methodList := map[string]interface{}{"post": nil, "get": nil}
-	s := strings.ToLower(method)
-	if _, ok := methodList[s]; !ok {
-		return nil, errors.New("请求方法必须是post/get")
-	}
-	data := ""
-	if query != nil {
-		if params, ok := query.(map[string]string); ok {
-			i := 0
-			for k, v := range params {
-				if i > 0 {
-					data += "&"
-				}
-				data += fmt.Sprintf("%s=%s", k, v)
-				i++
-			}
-		} else {
-			data, ok = query.(string)
-			if !ok {
-				return nil, errors.New("参数类型不合法")
-			}
-		}
-	}
-	request, err := http.NewRequest(strings.ToUpper(method), url, strings.NewReader(data))
-	if err != nil {
-		return nil, err
-	}
-	defer request.Body.Close()
-	hasUserAgent := false
-	if headers != nil {
-		for hName, hVal := range headers {
-			if strings.ToLower(hName) == "user-agent" {
-				hasUserAgent = true
-			}
-			request.Header.Add(hName, hVal)
-		}
-	}
-	if !hasUserAgent {
-		request.Header.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36")
-	}
-	response, err := httpClient.Do(request)
-	if err != nil {
-		return nil, err
-	}
-	defer response.Body.Close()
-	if response.StatusCode != 200 {
-		if retry > 0 {
-			fmt.Printf("第%d次尝试\n", retry)
-			retry = retry - 1
-			return Curl(method, url, query, headers)
-		}
-	}
-	res, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return nil, err
-	}
-	return res, nil
 }
